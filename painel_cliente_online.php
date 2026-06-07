187693d4605a5691fd19aa7348468046463317ff
BRENOP24 <brenocosma@gmail.com>
2026-06-07 13:16:44 -0300

    Update painel_cliente_online.php

--- painel_cliente_online.php
+++ painel_cliente_online.php
@@ -69,7 +69,7 @@ $historico = $stmtHist->fetchAll(PDO::FETCH_ASSOC);
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
-        <a class="navbar-brand text-wrap" style="max-width: 60%; font-size: 1rem;" href="#">SAY NOW</a>
+        <a class="navbar-brand text-wrap" style="max-width: 60%; font-size: 1rem;" href="#">SAY NOW Pedidos Conluidos + Andamento </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarPainel">
             <span class="navbar-toggler-icon"></span>
         </button>
